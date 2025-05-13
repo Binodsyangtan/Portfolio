9ca4c77327d36d74360badf2f95f3022b9c5c2a3
Binodsyangtan <binodsyangtan9761@gmail.com>
2025-05-13 17:18:37 +0545

    complete

---
+++ src/components/Experience/Experience.jsx
@@ -0,0 +1,22 @@
+// import React from "react";
+// import { experiences } from "../../constants"; // Import your data
+
+// const Experience = () => {
+//   return (
+//     <section
+//       id="experience"
+//       className="py-24 pb-24 px-[12vw] md:px-[7vw] lg:px-[16vw] font-sans bg-skills-gradient clip-path-custom-2"
+//     >
+//       {/* Section Title */}
+//       <div className="text-center mb-16">
+//         <h2 className="text-4xl font-bold text-white">EXPERIENCE</h2>
+//         <div className="w-32 h-1 bg-[#00BFFF] mx-auto mt-4"></div>
+//         <p className="text-gray-400 mt-4 text-lg font-semibold">
+//           Full Stack Developer | Open to Internship Roles
+//         </p>
+//       </div>
+//     </section>
+//   );
+// };
+
+// export default Experience;
---
+++ src/constants.js
@@ -0,0 +1,201 @@
+// Skills Section Logo's
+import htmlLogo from './assets/tech_logo/html.png';
+import cssLogo from './assets/tech_logo/css.png';
+import javascriptLogo from './assets/tech_logo/javascript.png';
+import reactjsLogo from './assets/tech_logo/reactjs.png';
+// import reduxLogo from './assets/tech_logo/redux.png';
+import nextjsLogo from './assets/tech_logo/nextjs.png';
+import tailwindcssLogo from './assets/tech_logo/tailwindcss.png';
+// import materialuiLogo from './assets/tech_logo/materialui.png';
+import bootstrapLogo from './assets/tech_logo/bootstrap.png';
+import nodejsLogo from './assets/tech_logo/nodejs.png';
+import expressjsLogo from './assets/tech_logo/express.png';
+import mysqlLogo from './assets/tech_logo/mysql.png';
+import mongodbLogo from './assets/tech_logo/mongodb.png';
+import firebaseLogo from './assets/tech_logo/firebase.png';
+import cLogo from './assets/tech_logo/c.png';
+import cppLogo from './assets/tech_logo/cpp.png';
+import javaLogo from './assets/tech_logo/java.png';
+// import pythonLogo from './assets/tech_logo/python.png';
+import gitLogo from './assets/tech_logo/git.png';
+import githubLogo from './assets/tech_logo/github.png';
+import vscodeLogo from './assets/tech_logo/vscode.png';
+import postmanLogo from './assets/tech_logo/postman.png';
+import mcLogo from './assets/tech_logo/mc.png';
+import figmaLogo from './assets/tech_logo/figma.png';
+import netlifyLogo from './assets/tech_logo/netlify.png';
+import vercelLogo from './assets/tech_logo/vercel.png';
+import postgreLogo from './assets/tech_logo/postgre.png';
+import csharpLogo from './assets/tech_logo/csharp.png';
+
+// // Experience Section Logo's
+
+// Education Section Logo's
+import amritLogo from './assets/education_logo/amrit.png';
+import vsniketan from './assets/education_logo/vsniketan.png';
+import geetaLogo from './assets/education_logo/geetalogo.png';
+
+// Project Section Logo's
+import sampannaHomes from './assets/work_logo/sampanna-homes.png'
+import portfolio from './assets/work_logo/portfolio.png'
+
+
+export const SkillsInfo = [
+  {
+    title: 'Frontend',
+    skills: [
+      { name: 'HTML', logo: htmlLogo },
+      { name: 'CSS', logo: cssLogo },
+      { name: 'JavaScript', logo: javascriptLogo },
+      { name: 'React JS', logo: reactjsLogo },
+      { name: 'Next JS', logo: nextjsLogo },
+      { name: 'Tailwind CSS', logo: tailwindcssLogo },
+      { name: 'Bootstrap', logo: bootstrapLogo },
+    ],
+  },
+  {
+    title: 'Backend',
+    skills: [
+      { name: 'Node JS', logo: nodejsLogo },
+      { name: 'Express JS', logo: expressjsLogo },
+      { name: 'MySQL', logo: mysqlLogo },
+      { name: 'MongoDB', logo: mongodbLogo },
+      { name: 'Firebase', logo: firebaseLogo },
+      { name: 'PostgreSQL', logo: postgreLogo },
+    ],
+  },
+  {
+    title: 'Languages',
+    skills: [
+      { name: 'C', logo: cLogo },
+      { name: 'C++', logo: cppLogo },
+      { name: 'Java', logo: javaLogo },
+      { name: 'C-Sharp', logo: csharpLogo },
+      { name: 'JavaScript', logo: javascriptLogo },
+    ],
+  },
+  {
+    title: 'Tools',
+    skills: [
+      { name: 'Git', logo: gitLogo },
+      { name: 'GitHub', logo: githubLogo },
+      { name: 'VS Code', logo: vscodeLogo },
+      { name: 'Postman', logo: postmanLogo },
+      { name: 'Compass', logo: mcLogo },
+      { name: 'Vercel', logo: vercelLogo },
+      { name: 'Netlify', logo: netlifyLogo },
+      { name: 'Figma', logo: figmaLogo },
+    ],
+  },
+];
+
+  // export const experiences = [
+  //   {
+  //     id: 0,
+  //     img: webverseLogo,
+  //     role: "Fullstack Developer",
+  //     company: "Webverse Digital",
+  //     date: "April 2024 - Present",
+  //     desc: "Developed dynamic and scalable web applications using the MERN stack, handling both frontend and backend development. Collaborated with cross-functional teams to build responsive UI, implement RESTful APIs, and optimize application performance in an agile environment.",
+  //     skills: [
+  //       "HTML",
+  //       "CSS",
+  //       "JavaScript",
+  //       "React JS",
+  //       "TypeScript",
+  //       "Node JS",
+  //       "Tailwind CSS",
+  //       "MongoDb",
+  //       "Redux",
+  //       " Next Js",
+  //     ],
+  //   },
+  //   {
+  //     id: 1,
+  //     img: agcLogo,
+  //     role: "Fullstack Engineer",
+  //     company: "Agumentik Group of Companies",
+  //     date: "July 2023 - March 2024",
+  //     desc: "Contributed to innovative projects as a Fullstack Engineer, leading both frontend and backend development using technologies such as HTML, CSS, JavaScript, PHP, SQL, Bootstrap, and ReactJS. Worked closely with the team to deliver responsive, high-performance web applications and improve user experience through seamless integration of various technologies.",
+  //     skills: [
+  //       "ReactJS",
+  //       "Redux",
+  //       "JavaScript",
+  //       "Tailwind CSS",
+  //       "HTML",
+  //       "CSS",
+  //       "SQL",
+  //     ],
+  //   },
+  //   {
+  //     id: 2,
+  //     img: newtonschoolLogo,
+  //     role: "Frontend Intern",
+  //     company: "Newton School",
+  //     date: "September 2021 - August 2022",
+  //     desc: "Worked as a Frontend Developer Intern, designing and implementing scalable UI components and responsive websites using HTML, CSS, JavaScript, Bootstrap, and Material UI. Collaborated with the design team to translate wireframes and prototypes from Figma into interactive, user-friendly web pages.",
+  //     skills: [
+  //       "HTML",
+  //       "CSS",
+  //       "Javascript",
+  //       "Bootstrap",
+  //       "Figma",
+  //       "Material UI",
+  //     ],
+  //   },
+  // ];
+  
+  export const education = [
+    {
+      id: 0,
+      img: amritLogo,
+      school: "Amrit Campus-Tribhuvan University",
+      date: "Currently Pursuing (7th sem)",
+      // grade: "73.2%",
+      desc: "I am currently pursuing a BSc. CSIT degree, which is a four-year program that integrates the core areas of computer science with practical aspects of information technology. The curriculum includes subjects such as programming, data structures, algorithms, database systems, web development, networking, and software engineering. This degree is helping me build a strong foundation in both theoretical concepts and real-world application, preparing me for roles in software development, system design, and IT solutions.",
+      degree: " Bachelor of Science in Computer Science and Information Technology(CSIT)-TU",
+    },
+    {
+      id: 1,
+      img: vsniketan,
+      school: "Vishwa Niketan Sec. School,Tripureswor, ward No. 11-kathmandu",
+      date: "2078 B.S (2021 A.D) year of completion",
+      grade: "B+",
+      desc: "I completed my class 12 education from Vishwa Niketan Sec. School,t Tripureswor, ward No. 11-kathmandu, under the NEB board, where I studied Physics, Chemistry, Mathematics, C.English and C.Nepali (Biolody in 11) ",
+      degree: "Science",
+    },
+    {
+      id: 2,
+      img: geetaLogo,
+      school: "Shree Geeta Mata Secondary School,Dallu-kathmandu",
+      date: "2075 B.S (2019 A.D) year of completion " ,
+      grade: "A",
+      desc: "I completed my class 10 education from Shree Geeta Mata Secondary School,Dallu-kathamandu",
+      // degree: "CBSE(X), Science with Computer Application",
+    },
+  ];
+  
+  export const projects = [
+    {
+      id: 0,
+      title: "Sampanna Homes(E-commerce)",
+      description:
+        "Sampanna Homes is a full-stack eCommerce web application built using the MERN stack (MongoDB, Express.js, React, Node.js). The frontend is developed with React and styled using Tailwind CSS, offering a responsive and user-friendly interface. It includes features like product listing, shopping cart, user authentication, and a blog section. The backend, powered by Node.js and Express, handles RESTful APIs and secure user management with JWT authentication. MongoDB is used as the database to store product, user, and order information. The project also includes an admin panel for managing products and blogs. The site is deployed using Netlify for the frontend and Render for the backend.",
+      image: sampannaHomes,
+      tags: ["HTML", "Tailwind CSS", "React JS", "ExpressJs","Nodejs","mongoDB"],
+      github: "https://github.com/Binodsyangtan/Sampanna-Homes",
+      webapp: "https://sampanna-homes.netlify.app",
+    },
+    {
+      id: 1,
+      title: "Portfolio project",
+      description:
+        "My Portfolio Website is a personal web application built using React.js for a dynamic and component-based UI. It is styled with Tailwind CSS and HTML, ensuring a clean, responsive, and mobile-friendly design. The site showcases personal information, projects, skills, and contact details in an interactive format. Various npm packages are integrated to enhance functionality and animations, such as smooth scrolling, form handling, and icon libraries. The portfolio is optimized for performance and user experience, reflecting modern web development best practices.",
+      image: portfolio,
+      tags: ["HTML", "Tailwind CSS", "React JS", "Npm-packages"],
+      github: "https://github.com/Binodsyangtan/Sampanna-Homes",
+      webapp: "https://sampanna-homes.netlify.app",
+    },
+    
+
+  ];  
